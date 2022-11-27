2fc29af1fdb0bc8ecb87ea9dc464ed55719b4b36
Soares <soares_123@msn.com>
2022-11-28 00:46:20 +0200

    Button example

---
+++ config.py
@@ -0,0 +1,2 @@
+flight_origin = "-123.414, 37.776"
+flight_destination = "24.945, 60.192"
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from game import *
+import config
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        if request.form.get('San Francisco') == 'San Francisco':
+            config.flight_origin = config.flight_destination
+            config.flight_destination = "-77.032, 38.913"
+    return render_template('index.html', flight_origin=config.flight_origin, flight_destination=config.flight_destination)
 
-app.run(use_reloader=True, debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(use_reloader=True, debug=True, host='0.0.0.0', port=5000)
