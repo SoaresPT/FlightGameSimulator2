64d5a5cf4f08bd2b9e40057ae9effcbba606935f
Soares <soares_123@msn.com>
2022-11-29 23:16:29 +0200

    Add button handler/logic

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-flight_origin = "-123.414, 37.776"
+flight_origin = "-122.414, 37.776"
 flight_destination = "24.945, 60.192"
--- main.py
+++ main.py
@@ -6,9 +6,20 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        if request.form.get('San Francisco') == 'San Francisco':
+        country= request.form['data']
+        print(f"country: {country}")
+        if country == "New York":
+            print(f"Triggered")
             config.flight_origin = config.flight_destination
+            print(f"Origin: {config.flight_origin}")
             config.flight_destination = "-77.032, 38.913"
+            print(f"Destination: {config.flight_destination}")
+        if country == "San Francisco":
+            print(f"Triggered")
+            config.flight_origin = config.flight_destination
+            print(f"Origin: {config.flight_origin}")
+            config.flight_destination = "-122.414, 37.776"
+            print(f"Destination: {config.flight_destination}")        
     return render_template('index.html', flight_origin=config.flight_origin, flight_destination=config.flight_destination)
 
 if __name__ == "__main__":
